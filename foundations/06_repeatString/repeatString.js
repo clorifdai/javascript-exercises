d608654ed1d06589253c43ec4a8f710bdb0807ad
Arif <153439486+clorifdai@users.noreply.github.com>
2026-02-23 04:04:13 +0800

    Solved 06-repeatString

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(word, repeat_num) {
+  if (repeat_num < 0) {
+    return "ERROR";
+  };
+  let word2 = "";
+  for ( let i = 0 ; i < repeat_num; i++) {
+    word2 += word
+  }
+  return word2;
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
