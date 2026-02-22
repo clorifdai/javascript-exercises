a6f4e0d13b260d1d60a59412b6341cbca1cfdd7d
Arif <153439486+clorifdai@users.noreply.github.com>
2026-02-23 04:43:06 +0800

    Solved 09_sumAll

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,6 +1,17 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+  let sum = 0;
+  let arr_num = [];
+  for (let i = a; i < b + 1; i++ ) {
+    arr_num.push(i);
+  }
+  for (arr_num_l of arr_num) {
+    sum += arr_num_l;
+    console.log(sum)
+  }
 
+  return sum;
 };
-
+// x = sumAll(1, 4)
+// console.log(x)
 // Do not edit below this line
 module.exports = sumAll;
