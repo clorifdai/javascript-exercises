456df3ebd0cbd858e1906cade18c0877d8717171
Arif <153439486+clorifdai@users.noreply.github.com>
2026-02-23 04:25:24 +0800

    Solved 08_removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(my_array, remove_element) {
+  // Cari remove element pada array
+  let index = my_array.indexOf(remove_element);
+  if (index > -1) {
+    my_array.splice(index, 1);
+  }
+  return my_array;
 };
 
 // Do not edit below this line
