d6565fe03105e8a3351653170b70404abbad3829
Arif <153439486+clorifdai@users.noreply.github.com>
2026-02-23 05:10:09 +0800

    Solved 10_leapYears

--- foundations/10_leapYears/leapYears.js
+++ foundations/10_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
-
+const leapYears = function (year) {
+  const empatTahun = year % 4 === 0;
+  const seratusTahun = year % 100 === 0;
+  const empatRatusTahun = year % 400 === 0; 
+  
+  if (empatTahun && (!seratusTahun || empatRatusTahun)) {
+    return true;
+  } else {
+    return false
+  }
 };
 
 // Do not edit below this line
