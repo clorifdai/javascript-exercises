55c712045ca0f5299013fb616e6d86a00e683736
Arif <153439486+clorifdai@users.noreply.github.com>
2026-02-23 04:10:58 +0800

    Solved 07_reverseString

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(sentence) {
+  let splitSentence = sentence.split("");
+  let reverseArray = splitSentence.reverse();
+  let joinArray = reverseArray.join("");
+  return joinArray;
 };
 
 // Do not edit below this line
