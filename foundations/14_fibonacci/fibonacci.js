b8e573209224fd2681043f9ab3ef59e347a7892a
Arif <153439486+clorifdai@users.noreply.github.com>
2026-03-05 00:51:33 +0800

    passed 4th fibonacci number is 3, 14_fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,6 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+  a = 0;
+  b = 1;
 
+  // Less than 0
+  if (num < 0) {
+    return "OOPS";
+  } else if (num === 0) {
+    return 0;
+  } else if (num == 1) {
+    return b;
+  } else {
+    for ( i = 1; i < num; i++) {
+      let c = a + b;
+      a = b;
+      b = c;
+    }
+    return b
+  }
 };
+
+// console.log (fibonacci(9));
 
 // Do not edit below this line
 module.exports = fibonacci;
