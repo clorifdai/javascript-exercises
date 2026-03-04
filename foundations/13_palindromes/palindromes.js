2753729d79a2c33039bc772ea513151adb08212c
Arif <153439486+clorifdai@users.noreply.github.com>
2026-03-04 23:10:30 +0800

    palindromes test1-2

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,21 @@
-const palindromes = function () {
+const palindromes = function (word) {
+  let left = 0, right = word.length - 1;
+  let word_copy = word.slice(0);
+  word_copy = word_copy.split("");
 
+  while (left < right) {
+    [word_copy[left], word_copy[right]] = [word_copy[right], word_copy[left]];
+    left++;
+    right--;
+  }
+  word_copy = word_copy.join("")
+  if (word === word_copy) {
+    return true;
+  } else {
+    return false;
+  };
+    
+  
 };
 
 // Do not edit below this line
