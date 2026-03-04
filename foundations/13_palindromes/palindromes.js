f786eaa2f6479ecac1f095a6800be25705e8ea8e
Arif <153439486+clorifdai@users.noreply.github.com>
2026-03-04 23:14:59 +0800

    solve in a simple way  palindromes test1-2

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,22 +1,19 @@
 const palindromes = function (word) {
-  let left = 0, right = word.length - 1;
   let word_copy = word.slice(0);
-  word_copy = word_copy.split("");
-
-  while (left < right) {
-    [word_copy[left], word_copy[right]] = [word_copy[right], word_copy[left]];
-    left++;
-    right--;
-  }
-  word_copy = word_copy.join("")
-  if (word === word_copy) {
-    return true;
-  } else {
-    return false;
-  };
-    
+    let res = [];
   
-};
+    for (let i = word_copy.length - 1; i >= 0; i--) {
+        res.push(word_copy[i]);
+    }
+    word_copy = res.join('');
+
+    if (word_copy === word) {
+      return true;
+    } else {
+      return false;
+    }
+
+  };
 
 // Do not edit below this line
 module.exports = palindromes;
