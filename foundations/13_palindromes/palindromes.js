65d566dcb842e7c1d38dce5c2369173687dea3f3
Arif <153439486+clorifdai@users.noreply.github.com>
2026-03-04 23:24:06 +0800

    fix identation

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,20 +1,20 @@
 const palindromes = function (word) {
   word_low = word.toLowerCase()
   let word_low_copy = word_low.slice(0);
-    let res = [];
+  let res = [];
   
-    for (let i = word_low_copy.length - 1; i >= 0; i--) {
-        res.push(word_low_copy[i]);
-    }
-    word_copy = res.join('');
+  for (let i = word_low_copy.length - 1; i >= 0; i--) {
+      res.push(word_low_copy[i]);
+  }
+  word_copy = res.join('');
 
-    if (word_low_copy === word_low) {
-      return true;
-    } else {
-      return false;
-    }
+  if (word_low_copy === word_low) {
+    return true;
+  } else {
+    return false;
+  }
 
-  };
+};
 
 // Do not edit below this line
 module.exports = palindromes;
