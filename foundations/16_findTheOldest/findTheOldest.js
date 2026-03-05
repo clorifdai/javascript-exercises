0f43605f3ab012d67632939c2911b12e69dbfd4d
Arif <153439486+clorifdai@users.noreply.github.com>
2026-03-05 15:43:03 +0800

    finds the person with the greatest age!, 16_getTheTitles

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,35 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+  let name_oldest = "";
+  let arr_age = [];
+  for (person of arr) {
+    let age = person.yearOfDeath - person.yearOfBirth;
+    arr_age.push(age);
+  }
+  let oldest = Math.max(...arr_age);
+  let index_oldest = arr_age.indexOf(oldest);
+  
+  return arr[index_oldest];
 };
+
+// const people = [
+//   {
+//     name: "Carly",
+//     yearOfBirth: 1942,
+//     yearOfDeath: 1970,
+//   },
+//   {
+//     name: "Ray",
+//     yearOfBirth: 1962,
+//     yearOfDeath: 2011,
+//   },
+//   {
+//     name: "Jane",
+//     yearOfBirth: 1912,
+//     yearOfDeath: 1941,
+//   },
+// ]
+
+// console.log(findTheOldest(people));
 
 // Do not edit below this line
 module.exports = findTheOldest;
