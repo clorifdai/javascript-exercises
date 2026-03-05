66db82bb1f3ac74e4bf4bae551c09c0d1e79bd4d
Arif <153439486+clorifdai@users.noreply.github.com>
2026-03-05 15:11:01 +0800

    Access array of object, 15_getTheTitles

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,6 +1,23 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(arr) {
+  let arr_hel = [];
+  for (arr_l of arr) {
+    arr_hel.push(arr_l.title);
+  }
+  return arr_hel
 };
+
+// const books = [
+//   {
+//     title: 'Book',
+//     author: 'Name'
+//   },
+//   {
+//     title: 'Book2',
+//     author: 'Name2'
+//   }
+// ];
+
+// console.log(getTheTitles(books));
 
 // Do not edit below this line
 module.exports = getTheTitles;
